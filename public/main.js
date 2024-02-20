fa4272f2b66b2db1dc6c34b7c1773b64cbac4121
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2024-02-21 01:45:33 +0530

    refracted

--- public/main.js
+++ public/main.js
@@ -3,34 +3,34 @@ function ctaClick() {
     window.location.href = "https://www.rightamericanflag.com/cfv1/?affId=CDB7208E&c1=11458&c2=&sub5=&oid=1928&affid=11458&gtm_debug=1707756768489";
 }
 
-// OptBtn1 click event
-document.querySelectorAll(".optBtn1").forEach(btn => {
-    btn.addEventListener("click", function () {
-        this.parentElement.parentElement.nextElementSibling.style.display = "block";
-        this.parentElement.parentElement.style.display = "none";
-        window.scrollTo(0, 0);
-    });
-});
+// // OptBtn1 click event
+// document.querySelectorAll(".optBtn1").forEach(btn => {
+//     btn.addEventListener("click", function () {
+//         this.parentElement.parentElement.nextElementSibling.style.display = "block";
+//         this.parentElement.parentElement.style.display = "none";
+//         window.scrollTo(0, 0);
+//     });
+// });
 
-// Back-btn-actv click event
-document.querySelectorAll(".back-btn-actv").forEach(btn => {
-    btn.addEventListener("click", function () {
-        this.parentElement.previousElementSibling.style.display = "block";
-        this.parentElement.style.display = "none";
-        window.scrollTo(0, 0);
-    });
-});
+// // Back-btn-actv click event
+// document.querySelectorAll(".back-btn-actv").forEach(btn => {
+//     btn.addEventListener("click", function () {
+//         this.parentElement.previousElementSibling.style.display = "block";
+//         this.parentElement.style.display = "none";
+//         window.scrollTo(0, 0);
+//     });
+// });
 
-// Next-btn click event
-document.querySelectorAll(".next-btn").forEach(btn => {
-    btn.addEventListener("click", () => {
-        document.querySelector(".step2").style.display = "none";
-        document.querySelector(".step3").style.display = "block";
-        window.scrollTo(0, 0);
-    });
-});
+// // Next-btn click event
+// document.querySelectorAll(".next-btn").forEach(btn => {
+//     btn.addEventListener("click", () => {
+//         document.querySelector(".step2").style.display = "none";
+//         document.querySelector(".step3").style.display = "block";
+//         window.scrollTo(0, 0);
+//     });
+// });
 
-// Use const for startTimer function
+// // Use const for startTimer function
 const startTimer = (duration) => {
     let timer = duration;
     let minutes, seconds;
@@ -51,7 +51,7 @@ const startTimer = (duration) => {
     }, 1000);
 };
 
-// helloworld function
+// // helloworld function
 const helloworld = () => {
     document.getElementById("click-below-text").innerHTML =
         `<br/><span style="font-weight:bold;color:red">IMPORTANT:</span> After you confirm your zip code on the next page <span style="font-weight:bold;color:red">we WILL contact you by phone within 15 minutes</span>, to verify your address & ensure your application is real.`;
@@ -65,40 +65,40 @@ const helloworld = () => {
     startTimer(60 * 10);
 };
 
-// Attach click event listeners to #q1 and #q65
-document.getElementById("q1").addEventListener("click", function () {
-    document.querySelector(".step0").style.display = "none";
-    document.querySelector(".step1").style.display = "block";
-});
+// // Attach click event listeners to #q1 and #q65
+// document.getElementById("q1").addEventListener("click", function () {
+//     document.querySelector(".step0").style.display = "none";
+//     document.querySelector(".step1").style.display = "block";
+// });
 
-document.getElementById("q65").addEventListener("click", function () {
-    document.querySelector(".step0").style.display = "none";
-    document.querySelector(".step1").style.display = "block";
-});
+// document.getElementById("q65").addEventListener("click", function () {
+//     document.querySelector(".step0").style.display = "none";
+//     document.querySelector(".step1").style.display = "block";
+// });
 
-// Initialize counting
-var countingItem = document.getElementById("countingItem");
-var seconds = 59;
-var minutes = 4;
+// // Initialize counting
+// var countingItem = document.getElementById("countingItem");
+// var seconds = 59;
+// var minutes = 4;
 
-function counting() {
-    seconds -= 1;
-    if (seconds == 0) {
-        minutes -= 1;
-        seconds = 60;
-    }
-    if (seconds > 0 && minutes >= 0) {
-        if (seconds < 10) {
-            countingItem.innerHTML = "0" + minutes + ":0" + seconds;
-        } else {
-            countingItem.innerHTML = "0" + minutes + ":" + seconds;
-        }
-        window.setTimeout(counting, 1000);
-    } else {
-        countingItem.textContent = "00:00";
-    }
-}
+// function counting() {
+//     seconds -= 1;
+//     if (seconds == 0) {
+//         minutes -= 1;
+//         seconds = 60;
+//     }
+//     if (seconds > 0 && minutes >= 0) {
+//         if (seconds < 10) {
+//             countingItem.innerHTML = "0" + minutes + ":0" + seconds;
+//         } else {
+//             countingItem.innerHTML = "0" + minutes + ":" + seconds;
+//         }
+//         window.setTimeout(counting, 1000);
+//     } else {
+//         countingItem.textContent = "00:00";
+//     }
+// }
 
-// Start counting
-counting();
+// // Start counting
+// counting();
 
