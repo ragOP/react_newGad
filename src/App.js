23b5c5d8e2e3665176ea20df4d551b0ecc0806ef
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2024-02-20 22:48:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe
+        title="abc.html"
+        src={'./abc.html'}
+        width="100%"
+        height="600px"
+        frameBorder="0"
+      />
+    </div>
+  );
+}
+
+export default App;
